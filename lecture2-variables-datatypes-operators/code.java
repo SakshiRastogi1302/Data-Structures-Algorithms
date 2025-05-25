4dcba424b7d9c85fcaf477dd69995826853c850c
Sakshi Rastogi <sakshi.rastogi1302@gmail.com>
2025-05-25 19:24:39 +0530

    Revert "print full name in java"
    This reverts commit a57ea7892e903411aabb8e870c29efb9507b1237.

--- lecture2-variables-datatypes-operators/code.java
+++
@@ -1,8 +0,0 @@
-
-
-public class code {
-    public static void main(String[] args) {
-        System.out.println("My name is Sakshi Rastogi.\nI am 25 years old.");
-        System.out.println("Sakshi\nRastogi");
-    }
-}
