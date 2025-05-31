37615302275e5b4ca41bd609b0e10992f7bb8464
Sakshi Rastogi <sakshi.rastogi1302@gmail.com>
2025-05-31 16:03:44 +0530

    inverted triangle 2 in java
    written code in java that prints the following inverted triangle pattern 2.
    
    AAAA
       BBB
         CC
           D

---
+++ lecture4-patterns/InvertedTrianglePattern2.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class InvertedTrianglePattern2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number :- ");
+        int n = sc.nextInt();
+        char c = 'A';
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = i; j < n; j++) {
+                System.out.print(c);
+            }
+            c = (char) (c + 1);
+            
+            System.out.println();
+        }
+        sc.close();
+    }
+}
