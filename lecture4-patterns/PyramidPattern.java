329801eba1d9247eca757f29f0406065465c95cf
Sakshi Rastogi <sakshi.rastogi1302@gmail.com>
2025-05-31 16:19:29 +0530

    pyramid pattern in java
    written code in java that prints the following pyramid pattern.
    
    _ _ _ 1
    _ _ 1 2 1
    _ 1 2 3 2 1
    1 2 3 4 3 2 1

---
+++ lecture4-patterns/PyramidPattern.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class PyramidPattern {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number :- ");
+        int n = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                System.out.print(" ");
+            }
+            
+            for (int k = 1; k <= i + 1; k++) {
+                System.out.print(k);
+            }
+            
+            for (int l = i; l > 0; l--) {
+                if (i != 0) {
+                    System.out.print(l);
+                }
+            }
+            
+            System.out.println();
+
+
+        }
+
+        sc.close();
+        
+    }
+}
