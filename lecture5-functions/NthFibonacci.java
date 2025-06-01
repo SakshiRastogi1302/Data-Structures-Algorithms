0b97f3fcee10426bf1b232d677f1790942d07ead
Sakshi Rastogi <sakshi.rastogi1302@gmail.com>
2025-06-01 17:20:02 +0530

    fibonacci number in java.
    written a code in java that prints the nth fibonacci number where n is the input taken from the user.

---
+++ lecture5-functions/NthFibonacci.java
@@ -0,0 +1,33 @@
+
+import java.sql.Blob;
+import java.util.Scanner;
+
+public class NthFibonacci {
+    public static void printNthFibonacci(int n) {
+        if (n == 0 || n == 1) {
+            System.out.println(n);
+            return;
+        }
+
+        int a = 0;
+        int b = 1;
+        int c = 0;
+
+        for (int i = 2; i <= n; i++) {
+            c = a + b;
+            a = b;
+            b = c;
+
+        }
+
+        System.out.println(c);
+
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number :- ");
+        int n = sc.nextInt();
+        printNthFibonacci(n);
+        sc.close();
+    }
+}
