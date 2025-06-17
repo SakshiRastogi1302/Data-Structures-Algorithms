efcd54562ad71eec17dead44a3dbb4a7fa6e249d
Sakshi Rastogi <sakshi.rastogi1302@gmail.com>
2025-06-17 16:55:07 +0530

    added one print statement

--- lecture2-variables-datatypes-operators/Code.java
+++ lecture2-variables-datatypes-operators/Code.java
@@ -1,5 +1,6 @@
 public class Code {
     public static void main(String[] args) {
+        System.out.println("Sakshi Rastogi");
         System.out.println("My name is Sakshi Rastogi.\nI am 25 years old.");
         System.out.println("Sakshi\nRastogi");
     }
